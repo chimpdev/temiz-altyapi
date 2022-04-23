a0b27895457a94eaae8f9d8c72dcac698c3c4bdf
Gökhan Can <55600314+chimpdev@users.noreply.github.com>
2022-04-23 21:07:08 +0300

    Update modalSubmit.js

--- library/events/modalSubmit.js
+++ library/events/modalSubmit.js
@@ -8,9 +8,9 @@ module.exports = async interaction => {
 
     try {
       const result = await eval(code.includes('await') ? code : `(async () => { ${code} })()`);
-      return interaction.followUp({ content: '```js\n' + inspect(result, { depth: 0 }).slice(0, 1992) + '```' });
+      return interaction.followUp({ content: '```js\n' + inspect(result, { depth: 0 }).slice(0, 1990) + '```' });
     } catch(error) {
-      return interaction.followUp({ content: '```js\n' + inspect(error, { depth: 0 }).slice(0, 1992) + '```' });
+      return interaction.followUp({ content: '```js\n' + inspect(error, { depth: 0 }).slice(0, 1990) + '```' });
     };
 
   }; 
